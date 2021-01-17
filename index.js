34bb4f16abe5b798e9c2406a46aa56458eb10690
Sunil Pandey <inkingmind@gmail.com>
2021-01-17 20:34:28 +0530

    tst commit

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ module.exports = app => {
     // `context` extracts information from the event, which can be passed to
     // GitHub API calls. This will return:
     //   {owner: 'yourname', repo: 'yourrepo', number: 123, body: 'Hello World!}
-    const params = context.issue({ body: "He    llo GHApp!" });
+    const params = context.issue({ body: "He    llo GH    App!" });
 
     // Post a comment on the issue
     return context.github.issues.createComment(params);
